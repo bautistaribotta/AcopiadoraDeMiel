cf3bb06dd3afcb457a5d5a0006ba87608c5184f2
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-10-07 13:26:59 -0300

    Creacion del archivo main y su desarrollo, creo la ventana principal y las 3 opciones disponibles para el programa

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-import tkinter as tk
+import tkinter
+
+def abrir_ventana_clientes():
+    ventana_clientes = tkinter.Toplevel(ventana)
+    ventana_clientes.title("Clientes")
+    ventana_clientes.geometry("600x400+700+100")
+    ventana_clientes.configure(bg="white")
+
+def abrir_ventana_productos():
+    ventana_productos = tkinter.Toplevel(ventana)
+    ventana_productos.title("Productos")
+    ventana_productos.geometry("600x400+25+100")  # Tamaño y posición
+    ventana_productos.configure(bg="white")
+
+ventana = tkinter.Tk()
+
+# Obtener tamaño de pantalla
+ancho_pantalla = ventana.winfo_screenwidth()
+alto_pantalla = ventana.winfo_screenheight()
+
+# Configurar ventana principal
+ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}+0+0")
+ventana.title("Miel")
+ventana.iconbitmap(".gitignore\hogar.ico")
+ventana.configure(bg="gray16")
+ventana.resizable(True, True)
+ventana.attributes("-alpha", 1)
+
+# Encabezado superior con botones
+encabezado = tkinter.Frame(ventana, bg="gray25", height=50)
+encabezado.pack(fill="x")
+
+boton_productos = tkinter.Button(encabezado, text="Productos", command=abrir_ventana_productos)
+boton_productos.pack(side="left", padx=10, pady=10)
+
+boton_clientes = tkinter.Button(encabezado, text="Clientes", command=abrir_ventana_clientes)
+boton_clientes.pack(side="left", padx=10, pady=10)
+
+boton_remitos = tkinter.Button(encabezado, text="Remitos")
+boton_remitos.pack(side="left", padx=10, pady=10)
+
+# Frame principal
+frame1 = tkinter.Frame(ventana, bg="white", bd=5)
+frame1.pack(fill="both", expand=True)
+
+ventana.mainloop()
