4ece80abb9c7f6ebf14dc40e87017cdccc753284
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-10-14 16:29:48 -0300

    Agrego los nuevos datos a la clase Cliente

--- Cliente.py
+++ Cliente.py
@@ -1,16 +1,17 @@
 class Cliente:
     """Modelo de datos para Cliente"""
 
-    def __init__(self, nombre, localidad, direccion="", colmenas=0, renapa="", factura=False, id_db=None):
+    def __init__(self, nombre, email, localidad, telefono ,direccion="",
+                 colmenas=0, renapa="", factura=False, id_db=None):
         self.id_db = id_db  # ID de la base de datos
         self.nombre = nombre
-        self.email = f"{nombre.lower().replace(' ', '.')}@email.com"
+        self.email = email
         self.localidad = localidad
+        self.telefono = telefono
         self.direccion = direccion
         self.colmenas = colmenas
         self.renapa = renapa
         self.factura = factura
-        self.telefono = "+54 351 000-0000"
 
     def get_datos_tabla(self):
         """Retorna tupla para mostrar en tabla"""
@@ -22,13 +23,13 @@ class Cliente:
         # tupla_db: (id, nombre, email, telefono, localidad, direccion, colmenas, renapa, factura, fecha_registro)
         cliente = Cliente(
             nombre=tupla_db[1],
-            localidad=tupla_db[4],
+            email=tupla_db[2],
+            localidad=tupla_db[3],
+            telefono=tupla_db[4],
             direccion=tupla_db[5],
             colmenas=tupla_db[6],
             renapa=tupla_db[7],
             factura=bool(tupla_db[8]),
             id_db=tupla_db[0]
         )
-        cliente.email = tupla_db[2]
-        cliente.telefono = tupla_db[3]
         return cliente
