569d95b14b434c2d914204d4628d3cb50285542e
Bautista Ribotta <bautistaribotta@hotmail.com>
2025-10-25 12:53:47 -0300

    Resuelvo un bug del archivo VentanaNuevoCliente relacionado a la ruta de los archivos

--- gui/VentanaNuevoCliente.py
+++ gui/VentanaNuevoCliente.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
 from Cliente import Cliente
+import os
+import sys
+
+def resource_path(relative_path):
+    """Obtener la ruta absoluta del recurso"""
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 class VentanaNuevoCliente:
     """Ventana para registrar un nuevo cliente"""
